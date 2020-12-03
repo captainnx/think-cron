b4f61b222ea14e4fefd99043be4485535c18ff5c
captainnx <31986362+captainnx@users.noreply.github.com>
2020-12-03 16:38:04 +0800

    Update Task.php

--- src/cron/Task.php
+++ src/cron/Task.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Closure;
 use Cron\CronExpression;
 use DateTimeZone;
+use think\Exception;
 use think\Cache;
 
 abstract class Task
@@ -77,6 +78,8 @@ abstract class Task
 
         try {
             $this->execute();
+        } catch(Exception $e){
+            
         } finally {
             $this->removeMutex();
         }
